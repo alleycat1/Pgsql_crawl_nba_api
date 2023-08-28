ebb0432ac997db3c865444770f823d762fab5e43
netragonlong <netragonlong@gmail.com>
2023-08-28 14:32:07 +0900

    changed to avoid duplicated game id

--- main_sport_15.py
+++ main_sport_15.py
@@ -80,20 +80,20 @@ try:
     cursor.execute('DELETE FROM game_ids')
 
     for game in all_game_json.data_sets[0].data['data']:
-        insert_query = """
-            INSERT INTO game_ids (game_id, season_id, team_id, team_abbreviation, team_name, game_date)
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """
-        game_id = game[4]
-        season_id = game[0]
-        team_id = game[1]
-        team_abbreviation = game[2]
-        team_name = game[3]
-        game_date = game[5]
-        data = (game_id, season_id, team_id, team_abbreviation, team_name, game_date)
-        cursor.execute(insert_query, data)
-        if game_id not  in game_ids:
+        if game_id not in game_ids:
             game_ids.append(game_id)
+            insert_query = """
+                INSERT INTO game_ids (game_id, season_id, team_id, team_abbreviation, team_name, game_date)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            game_id = game[4]
+            season_id = game[0]
+            team_id = game[1]
+            team_abbreviation = game[2]
+            team_name = game[3]
+            game_date = game[5]
+            data = (game_id, season_id, team_id, team_abbreviation, team_name, game_date)
+            cursor.execute(insert_query, data)
     conn.commit()
 
     cursor.execute("BEGIN;")
